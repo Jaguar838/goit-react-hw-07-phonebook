06909a84bb7f8b0c3cc98da53c1a15ff24ccbc30
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-24 13:40:31 +0300

    Updates

---
+++ precache-manifest.9598e8cf0eb524b664ef89927564d24a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceccc50f14370d9048bd711065e22489",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "cc525b3c5d8704089c06",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.4d7bd101.chunk.css"
+  },
+  {
+    "revision": "14bd3b9c28d1cbbee3b0",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e878e6ab.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e878e6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc525b3c5d8704089c06",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.b96f7702.chunk.js"
+  },
+  {
+    "revision": "a3c64e46cc3f4f5c60e7",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.ccce52cd.js"
+  },
+  {
+    "revision": "c0227f343ce26ffa9c5dd7676ee441f6",
+    "url": "/goit-react-hw-03-phonebook/static/media/bg.c0227f34.png"
+  },
+  {
+    "revision": "33e07a1e468e0ce154769cab86e5d05a",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-500.33e07a1e.woff"
+  },
+  {
+    "revision": "b4c1a1dc7f990c3b92ae0ad6b180bbf8",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-500.b4c1a1dc.woff2"
+  },
+  {
+    "revision": "bbc96d50ab9d8291be82b05f64bfeb11",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-700.bbc96d50.woff2"
+  },
+  {
+    "revision": "dda8fc164a6a04a69538d3287a3d05e7",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-700.dda8fc16.woff"
+  },
+  {
+    "revision": "180cbc975448901234b7ff1d8b939b0d",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-800.180cbc97.woff"
+  },
+  {
+    "revision": "ffc5869427421361820e24d88f396f09",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-800.ffc58694.woff2"
+  },
+  {
+    "revision": "4cd0fd34f59af6827ed740223b5ce110",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-regular.4cd0fd34.woff2"
+  },
+  {
+    "revision": "b3e1c2f055c2c905173fa58205cd6ddd",
+    "url": "/goit-react-hw-03-phonebook/static/media/montserrat-v15-latin_cyrillic-regular.b3e1c2f0.woff"
+  }
+]);
